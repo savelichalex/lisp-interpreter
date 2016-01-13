e52d7a04baaea710e899ddf4e04e087f6427d011
savelichalex <savelichalex93@gmail.com>
2016-01-13 22:49:28 +0300

    Add apply and some help functions

--- core.js
+++ core.js
@@ -1,9 +1,30 @@
+function cons(first, second) {
+    return {
+        car: first,
+        cdr: second,
+        toString() {
+            return '[cons]'
+        }
+    }
+}
+
+function car(pair) {
+    return pair.car;
+}
+
+function cdr(pair) {
+    return pair.cdr;
+}
+
+function isPair(pair) {
+    return pair.toString() === '[cons]';
+}
+
 function cond(arr) {
     let length = arr.length;
-    for(let i = 0; i < length, i++) {
+    for(let i = 0; i < length; i++) {
         if(arr[i]()) {
-            arr[++i]();
-            break;
+            return arr[++i]();
         } else {
             i++;
         }
@@ -22,6 +43,102 @@ function eval(exp, env) {
         () => isBegin(exp), () => evalSequence(beginActions(exp), env),
         () => isCond(exp), () => eval(condToIf(exp), env),
         () => isApplication(exp), () => apply(eval(operator(exp), env), listOfValues(operands(exp), env)),
-        () => true, () => throw new Error('Eval: Unknown expression', exp)
+        () => true, () => {throw new Error('Eval: Unknown expression', exp)}
     ]);
+}
+
+function apply(procedure, arguments) {
+    cond([
+        () => isPrimitiveProcedure(procedure), () => applyPrimitiveProcedure(procedure, arguments),
+        () => isCompoundProcedure(procedure), () => evalSequence(
+                                                        procedureBody(procedure),
+                                                        extendEnvironment(
+                                                            procedureParameters(procedure),
+                                                            arguments,
+                                                            procedureEnvironment(procedure)
+                                                        )
+                                                    ),
+        () => true, () => {throw new Error('Apply: Unknown procedure type', procedure)}
+    ]);
+}
+
+function listOfValues(exps, env, arr = []) {
+    if(isNoOperands(exps)) {
+        return arr;
+    } else {
+        arr.push(eval(firstOperand(exps), env));
+        listOfValues(restOperand(exps), env, arr);
+    }
+}
+
+function evalIf(exp, env) {
+    if(isTrue(eval(ifPredicate(exp), env))) {
+        eval(ifConsequent(exp), env);
+    } else {
+        eval(ifAlternative(exp), env);
+    }
+}
+
+function isTrue(cond) {
+    return !!cond;
+}
+
+function evalSequence(exps, env) {
+    if(isLastExp(exps)) {
+        eval(firstExp(exps), env);
+    } else {
+        eval(firstExp(exps), env);
+        evalSequence(restExps(exps), env);
+    }
+}
+
+function evalAssignment(exp, env) {
+    setVariableValue(assignmentVariable(exp), eval(assignmentValue(exp), env), env);
+    return 'ok';
+}
+
+function evalDefinition(exp, env) {
+    defineVariable(definitionVariable(exp), eval(definitionValue(exp), env), env);
+    return 'ok';
+}
+
+function isSelfEvaluating(exp) {
+    cond([
+        () => isNumber(exp), () => true,
+        () => isString(exp), () => true,
+        () => true, () => false
+    ]);
+}
+
+function isNumber(exp) {
+    return /^\d+$/gi.test(exp);
+}
+
+function isString(exp) {
+    return false; //TODO: check strings in lisp
+}
+
+function isVariable(exp) {
+    return isSymbol(exp);
+}
+
+function isSymbol(exp) {
+    return /^[\d\w\?!\-]+$/gi.test(exp);
+}
+
+function isQuoted(exp) {
+    return isTaggedList(exp, 'quote');
+}
+
+function textOfQuotation(exp) {
+    return cdr(car(exp));
+}
+
+function isTaggedList(exp, tag) {
+    if(isPair(exp)) {
+        if(car(exp) === tag) {
+            return true;
+        }
+    }
+    return false;
 }
